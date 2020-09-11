0f0db834e0184fdcc208617ba8d6689acc16fd2c
sawit <sawit204@gmail.com>
2020-09-12 02:21:54 +0700

    commit before build

--- src/App.js
+++ src/App.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import './App.css';
 import Row from './Row';
-
+import Banner from "./Banner";
+import Navbar from "./Navbar"
 import requests from './requests';
 function App() {
   return (
     <div className="App">
-      <h1>NETFLIEX</h1>
-      <Row title="NETFLIIX ORIGINALS" fatchUrl={requests.fatchNetflixOriginals} />
-      <Row title="Tranding Now" fatchUrl={requests.fatchTrending} />
+      {/* Navbar */}
+      <Navbar/>
+      {/* Banner */}
+      <Banner/>
+      {/* ./Banner */}
+
+      <Row
+        title="ผลงานของ Netflix"
+        fatchUrl={requests.fatchNetflixOriginals}
+        isLargeRow={true}
+      />
+      <Row title="กำลังฮิต" fatchUrl={requests.fatchTrending} />
+      <Row title="ฮิตที่ Netflix" fatchUrl={requests.fatchTopReted} />
+      <Row title="ภาพยนตร์แอ็﻿ค﻿ชั่﻿น" fatchUrl={requests.fatchActionMovies} />
+      <Row title="คอมเมดี้" fatchUrl={requests.fatchComedyMovies} />
+      <Row title="ภาพยนตร์สยองขวัญ" fatchUrl={requests.fatchHorrorMovies} />
+      <Row title="รายการทีวีโรแมนติก" fatchUrl={requests.fatchRomanceMovies} />
+      <Row title="สารคดี" fatchUrl={requests.fatchDocumentaries} />
     </div>
   );
 }
--- src/Row.js
+++ src/Row.js
@@ -1,38 +1,74 @@
-import React,{useState,useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from './axios';
+import Youtube from 'react-youtube';
+import movieTrailer from 'movie-trailer'
+import './Row.css'
 
 const image_base_url = "https://image.tmdb.org/t/p/original/";
-const image_base_url_w220_h330 = "https://image.tmdb.org/t/p/w220_and_h330_face/";
+// const image_base_url_w220_h330 = "https://image.tmdb.org/t/p/w220_and_h330_face/";
 
+function Row({ title, fatchUrl, isLargeRow }) {
 
-function Row({title,fatchUrl}) {
+  const [movies, setMovies] = useState([]);
+  const [trailerUrl, setTrailerUrl] = useState("");
 
-    const [movies, setMovies] = useState([]);
-    useEffect(() => {
-        // effect
-        async function fatchData(){
-            const request = await axios.get(fatchUrl);
-           console.log(request.data.results); 
-           setMovies(request.data.results);
-           return request.data.results;
-        }
-        fatchData();
-    }, [fatchUrl]);
+  useEffect(() => {
+    // effect
+    async function fatchData() {
+      const request = await axios.get(fatchUrl);
+      setMovies(request.data.results);
+      return request;
+    }
+    fatchData();
+  }, [fatchUrl]);
 
-    return (
-        <div>
-            {/* title */}
-            <h2>{title}</h2>
-            <div className="row__poster">
-                {movies.map((movie,i) =>(
-                    <img key={i} src={`${image_base_url_w220_h330}${movie.poster_path}`} alt={movie.name}/>
-                ))}                
-            </div>
-            {/* Container Posters */}
+  const opts = {
+    height: "390",
+    width: "100%",
+    playerVars: {
+      autoplay: 1,
+    }
+  }
 
+  const handleClick = (movie) => {
+    console.log(movie.name);
+    if (trailerUrl) {
+      setTrailerUrl("");
+    } else {
+      movieTrailer(movie?.name || "")
+        .then((url) => {
+          const urlParams = new URLSearchParams(new URL(url).search);
+          // https://www.youtube.com/watch?v=XtMThy8QKqU&list=PL-J2q3Ga50oMQa1JdSJxYoZELwOJAXExP&index=4
+          setTrailerUrl(urlParams.get("v"));
+        }).catch((err) => {
+          console.log(err);
+        })
+    }
+  }
 
-        </div>
-    )
+  return (
+    <div className="row">
+      {/* title */}
+      <h2>{title}</h2>
+      <div className="row__posters">
+        {movies.map((movie, i) => (
+          <img
+            className={`row__poster ${isLargeRow && "row__posterLarge"}`}
+            key={i}
+            onClick={() => handleClick(movie)}
+            // onMouseOver={() => handleClick(movie)}
+            // onMouseOut={() => setTrailerUrl("")}
+            src={`${image_base_url}${isLargeRow ? movie.poster_path : movie.backdrop_path
+              }`}
+            alt={movie.name}
+          />
+        ))}
+        {/* Perview Vdo */}
+      </div>
+      {/* Container Posters */}
+      {trailerUrl && <Youtube videoId={trailerUrl} opts={opts} />}
+    </div>
+  );
 }
 
 export default Row
